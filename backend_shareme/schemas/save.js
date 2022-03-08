c210fcdf293b43a2ccc3fcdce0b1f4943d1bc877
hhasona <hhasona@gmail.com>
2022-03-08 16:10:10 +0200

    creating login page

---
+++ backend_shareme/schemas/save.js
@@ -0,0 +1,17 @@
+export default {
+  name: "save",
+  title: "save",
+  type: "document",
+  fields: [
+    {
+      name: "postedBy",
+      title: "PostedBy",
+      type: "postedBy",
+    },
+    {
+      name: "userId",
+      title: "UserId",
+      type: "string",
+    },
+  ],
+}
